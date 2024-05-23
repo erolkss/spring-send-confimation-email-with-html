49265ada03f87ffc7c1d3a62f694e3a1319e224a
Lucas Lima <lucaserolima@gmail.com>
2024-05-22 22:53:41 -0400

    feat: Create a Confirmation Entity which will store the token and registration confirmation of the new user

---
+++ src/main/java/br/com/ero/sendemailhtml/domain/Confirmation.java
@@ -0,0 +1,34 @@
+package br.com.ero.sendemailhtml.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "confirmations")
+public class Confirmation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String token;
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    private LocalDateTime createDate;
+    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "user_id")
+    private User user;
+
+    public Confirmation(User user){
+        this.user = user;
+        this.createDate = LocalDateTime.now();
+        this.token = UUID.randomUUID().toString();
+    }
+}
