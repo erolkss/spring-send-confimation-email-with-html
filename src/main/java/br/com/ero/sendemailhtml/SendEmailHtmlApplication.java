730c95a3625a40f89e0807cef956ad86d5b6cfc3
Lucas Lima <lucaserolima@gmail.com>
2024-05-22 22:45:05 -0400

    First Commit - Create Project - Send Email with HTML

---
+++ src/main/java/br/com/ero/sendemailhtml/SendEmailHtmlApplication.java
@@ -0,0 +1,13 @@
+package br.com.ero.sendemailhtml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendEmailHtmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendEmailHtmlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ero/sendemailhtml/SendEmailHtmlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ero.sendemailhtml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendEmailHtmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
